2d22f1e4f8879e9414045543e5ea4ed71a6a0ada
DmytroHryhorashenko <dimon09051991@gmail.com>
2026-01-12 00:39:51 +0200

    Update lesson_1.py

--- lesson_1/lesson_1.py
+++ lesson_1/lesson_1.py
@@ -1,2 +1,63 @@
+print('Hello World')
 
-print("Hello world", 12.1, "dfbxcf")
+
+age =20
+user1_age = 30
+user2_age = 30
+ADULT_THR = 18
+_do_not_use_this = 0
+if age >= ADULT_THR:
+    print("Adult")
+
+x = 2
+y = x + 10
+print(y)
+
+
+a = 1
+b = 2
+c = a + b + 10
+print(c)
+
+
+x = 8 **3 + 4*(2 + 2)
+print (x)
+
+
+
+s1 = 'Hello'
+s2 = 'World!'
+joined_strin = s1 + s2
+print (s1+ " " +s2)
+
+name = "Oleg"
+hello_string = f"Hello, {name}!"
+print(hello_string)
+
+
+age = 18
+adult1 = age >= 18
+
+age = 15
+adult2 = age >= 18
+print(adult1, adult2)
+
+
+a = 3
+b = 5
+c = a < b
+d = a > b
+a == b
+a != b
+print(c)
+print(d)
+print(a == b)
+print(a != b)
+
+
+a =  input("name: ")
+print(a)
+
+
+
+
